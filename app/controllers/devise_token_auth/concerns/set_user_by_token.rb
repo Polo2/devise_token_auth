9a4128f3dddea43ba1cecdfa8474ccb72c9bd2c1
Paul M2B <polo2bodi@gmail.com>
2019-04-15 11:29:49 +0200

    add debug to request and params

--- app/controllers/devise_token_auth/concerns/set_user_by_token.rb
+++ app/controllers/devise_token_auth/concerns/set_user_by_token.rb
@@ -59,7 +59,17 @@ module DeviseTokenAuth::Concerns::SetUserByToken
     # parse header for values necessary for authentication
     uid        = request.headers[uid_name] || params[uid_name]
     @token     ||= request.headers[access_token_name] || params[access_token_name]
+    puts "token"
+    puts request.headers[access_token_name]
+    puts "request.headers[access_token_name]"
+    puts params[access_token_name]
+    puts "params[access_token_name]"
     @client_id ||= request.headers[client_name] || params[client_name]
+    puts "client"
+    puts request.headers[client_name]
+    puts "request.headers[client_name]"
+    puts params[client_name]
+    puts "params[client_name]"
 
     # client_id isn't required, set to 'default' if absent
     @client_id ||= 'default'
